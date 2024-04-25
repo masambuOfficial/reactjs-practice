675f49ed79872635537487a2cda857de7a8dbcdb
Masambu Emmanuel <masambue11@gmail.com>
2024-04-25 17:35:51 +0300

    Working with useState in reactjs

---
+++ src/components/Counter.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+function Counter() {
+    let [count, setCount] = useState(0);
+
+    function increaseCount(){
+        setCount(count+1);
+    }
+
+    function decreaseCount(){
+        setCount(count-1);
+    }
+  return (
+    <div>
+        <h1>Counter App</h1>
+        <h2>{count}</h2>
+        <button onClick={decreaseCount}>-</button>
+        <button onClick={increaseCount}>+</button>
+    </div>
+  )
+}
+
+export default Counter
